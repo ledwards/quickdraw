a94a80f4bc915c538214da9494f87151854f838a
Lee Edwards <lee@root.vc>
2021-03-23 12:50:41 -0700

    WIP recycling works

--- lib/main.dart
+++ lib/main.dart
@@ -38,9 +38,8 @@ class _HomeScreenState extends State<HomeScreen> {
   List<SwDecklist> _allDecklists = [];
   String _currentSide = "Light";
   SwStack _currentStack;
-  SwDeck _currentDeck;
   SwStack _maybeStack;
-  SwStack _tempStack;
+  SwDeck _currentDeck;
 
   Future<List<SwCard>> _loadCards() async {
     List<SwCard> cards = [];
@@ -96,17 +95,16 @@ class _HomeScreenState extends State<HomeScreen> {
       loadedCards = res[0];
       loadedDecklists = res[1];
       final decklist = loadedDecklists[1];
-      return _loadStack(decklist.cardNames.getRange(0, 3).toList(), loadedCards,
+      return _loadStack(decklist.cardNames.getRange(0, 5).toList(), loadedCards,
           decklist.title);
     });
 
     setState(() {
-      _allCards = loadedCards;
-      _allDecklists = loadedDecklists;
-      _currentStack = stack;
-      _currentDeck = new SwDeck(_currentSide, [], 'Default Deck');
-      _maybeStack = new SwStack(_currentSide, [], 'Maybe Cards');
-      _tempStack = new SwStack(_currentSide, [], 'never seen by user');
+      this._allCards = loadedCards;
+      this._allDecklists = loadedDecklists;
+      this._currentStack = stack;
+      this._maybeStack = new SwStack(_currentSide, [], 'Maybe Cards');
+      this._currentDeck = new SwDeck(_currentSide, [], 'Default Deck');
     });
   }
 
@@ -114,15 +112,17 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     if (_currentStack == null) {
       return new Scaffold(
+        key: UniqueKey(),
         appBar: new AppBar(
           title: new Text("Loading..."),
         ),
       );
     } else {
-      return Scaffold(
+      return new Scaffold(
+        key: UniqueKey(),
         appBar: new AppBar(
           title: new Text(
-              "Stack: ${_currentStack.length} | Temp: ${_tempStack.length} | Deck: ${_currentDeck.length} | Maybe: ${_maybeStack.length}"),
+              "Stack: ${_currentStack.length} | Deck: ${_currentDeck.length} | Maybe: ${_maybeStack.length}"),
           backgroundColor: Colors.transparent,
         ),
         body: new Center(
@@ -139,18 +139,7 @@ class _HomeScreenState extends State<HomeScreen> {
               maxHeight: MediaQuery.of(context).size.width,
               minWidth: MediaQuery.of(context).size.width * 0.8,
               minHeight: MediaQuery.of(context).size.width * 0.8,
-              cardBuilder: (context, index) => Card(
-                // child: Image.network(_currentStack[index].imageUrl),
-                child: Transform(
-                    alignment: Alignment.center,
-                    transform: _currentStack[index].subType == 'Site'
-                        ? Matrix4.rotationZ(-pi / 2)
-                        : Matrix4.rotationZ(0),
-                    child: Image.network(_currentStack[index].imageUrl,
-                        alignment: Alignment.center)),
-                color: Colors.transparent,
-                shadowColor: Colors.transparent,
-              ),
+              cardBuilder: _cardBuilder,
               cardController: CardController(),
               swipeUpdateCallback:
                   (DragUpdateDetails details, Alignment align) {
@@ -171,57 +160,22 @@ class _HomeScreenState extends State<HomeScreen> {
               swipeCompleteCallback:
                   (CardSwipeOrientation orientation, int index) {
                 setState(() {
-                  print("CURR: ${_currentStack.cards.map((e) => e.title)}");
-                  print("TEMP: ${_tempStack.cards.map((e) => e.title)}");
-
-                  // There are probably other cases too where you can
-                  //end up in the fucked up sitatuion
-
-                  if (orientation != CardSwipeOrientation.RIGHT &&
-                      _currentStack.length == 2 &&
-                      _tempStack.length == 0) {
-                    print("***ITS GONNA FUCK UP IF YOU SWIPE RIGHT, NEXT***");
-                    // TODO: disable swipe right?
-                  }
-                  if (orientation == CardSwipeOrientation.RIGHT &&
-                      _currentStack.length == 1 &&
-                      _tempStack.length == 0) {
-                    print("***ITS FUCKING UP RN***");
-                    orientation = CardSwipeOrientation.RECOVER;
-                  }
-
+                  SwCard swipedCard = this._currentStack.removeAt(index);
                   switch (orientation) {
                     case CardSwipeOrientation.LEFT:
-                      SwCard swipedCard = _currentStack.removeAt(index);
                       break;
                     case CardSwipeOrientation.RIGHT:
-                      SwCard swipedCard = _currentStack.removeAt(index);
-                      _tempStack.add(swipedCard);
+                      _currentStack.add(swipedCard);
                       break;
                     case CardSwipeOrientation.UP:
-                      SwCard swipedCard = _currentStack.removeAt(index);
                       _currentDeck.add(swipedCard);
                       break;
                     case CardSwipeOrientation.DOWN:
-                      SwCard swipedCard = _currentStack.removeAt(index);
                       _maybeStack.add(swipedCard);
                       break;
                     case CardSwipeOrientation.RECOVER:
+                      _currentStack.insert(index, swipedCard);
                       break;
-                  }
-                });
-
-                // print(_currentStack.length);
-                // print(_tempStack.length);
-
-                setState(() {
-                  if (_currentStack.length == 0) {
-                    print('all done');
-                    for (int i = 0; i < _tempStack.length; i++) {
-                      _currentStack.insert(index + i, _tempStack.cards[i]);
-                      print("inserting into ${index + i}");
-                    }
-                    _tempStack.cards.clear();
                   }
                 });
               },
@@ -230,5 +184,19 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       );
     }
+  }
+
+  Widget _cardBuilder(context, index) {
+    return Card(
+      child: Transform(
+          alignment: Alignment.center,
+          transform: _currentStack[index].subType == 'Site'
+              ? Matrix4.rotationZ(-pi / 2)
+              : Matrix4.rotationZ(0),
+          child: Image.network(_currentStack[index].imageUrl,
+              alignment: Alignment.center)),
+      color: Colors.transparent,
+      shadowColor: Colors.transparent,
+    );
   }
 }
